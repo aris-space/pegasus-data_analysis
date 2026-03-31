39feff6f5f36b39cb5e5be129575e8952dfad461
nbartzsch <nicobartzsch@gmail.com>
2026-03-31 14:08:26 +0200

    added thrust plot and other minor changes

--- src/plotting_functions.py
+++ src/plotting_functions.py
@@ -40,11 +40,19 @@ def plot_by_sensor(df, title, ylabel, time_range=None, highlight_range=None, eve
         ev = events.copy()
         ev["time"] = pd.to_datetime(ev["time"], errors="coerce")
         ev["t_rel_s"] = (ev["time"] - x0_time).dt.total_seconds()
+        ev = ev.sort_values("t_rel_s")  
+        
+        last_plotted_time = None
+        min_distance = 0.1  
+        
         for _, evt in ev.iterrows():
             t = evt["t_rel_s"]
-            label = str(evt["value"]).replace("[SEQ] ", "")
-            ax.axvline(t, color="black", linestyle="--", linewidth=0.9, alpha=0.7)
-            ax.text(t, 0.98, label, transform=ax.get_xaxis_transform(), rotation=90, va="top", ha="right", fontsize=7, alpha=0.85)
+            # Only plot if it's the first event or far enough from the last plotted event
+            if last_plotted_time is None or (t - last_plotted_time) >= min_distance:
+                label = str(evt["value"]).replace("[SEQ] ", "")
+                ax.axvline(t, color="black", linestyle="--", linewidth=0.9, alpha=0.7)
+                ax.text(t, 0.98, label, transform=ax.get_xaxis_transform(), rotation=90, va="top", ha="right", fontsize=7, alpha=0.85)
+                last_plotted_time = t
 
     ax.set_xlabel("Time since sequence start [s]")
     ax.set_ylabel(ylabel)
@@ -76,10 +84,21 @@ def plot_total_massflow(df, time_range=None, highlight_range=None, events=None, 
 
     df["t_rel_s"] = (df["time"] - x0_time).dt.total_seconds()
 
+    # Fixed colors for specific OSS and FSS
+    sensor_colors = {
+        "OSS_MF": "tab:blue",
+        "FSS_MF": "tab:orange",
+    }
     # Plot sensor values
     fig, ax = plt.subplots(figsize=(10, 4))
     for sid, group in df.groupby("sensorId"):
-        ax.plot(group["t_rel_s"], group["value"], label=str(sid))
+        sid_str = str(sid)
+        ax.plot(
+            group["t_rel_s"],
+            group["value"],
+            label=sid_str,
+            color=sensor_colors.get(sid_str, "gray")  # fallback for other sensors
+        )
 
     # Plot total mf by adding all mf together
     df_total_mf = df.dropna(subset=["time", "sensorId", "value"]).sort_values("time")
@@ -117,11 +136,19 @@ def plot_total_massflow(df, time_range=None, highlight_range=None, events=None, 
         ev = events.copy()
         ev["time"] = pd.to_datetime(ev["time"], errors="coerce")
         ev["t_rel_s"] = (ev["time"] - x0_time).dt.total_seconds()
+        ev = ev.sort_values("t_rel_s")  
+        
+        last_plotted_time = None
+        min_distance = 0.1  
+        
         for _, evt in ev.iterrows():
             t = evt["t_rel_s"]
-            label = str(evt["value"]).replace("[SEQ] ", "")
-            ax.axvline(t, color="black", linestyle="--", linewidth=0.9, alpha=0.7)
-            ax.text(t, 0.98, label, transform=ax.get_xaxis_transform(), rotation=90, va="top", ha="right", fontsize=7, alpha=0.85)
+            # Only plot if it's the first event or far enough from the last plotted event
+            if last_plotted_time is None or (t - last_plotted_time) >= min_distance:
+                label = str(evt["value"]).replace("[SEQ] ", "")
+                ax.axvline(t, color="black", linestyle="--", linewidth=0.9, alpha=0.7)
+                ax.text(t, 0.98, label, transform=ax.get_xaxis_transform(), rotation=90, va="top", ha="right", fontsize=7, alpha=0.85)
+                last_plotted_time = t
 
     ax.set_xlabel("Time since sequence start [s]")
     ax.set_ylabel("Total Massflow [g/s]")
@@ -256,7 +283,7 @@ def plot_mass_flux(df, time_range=None, highlight_range=None, events=None, x0_ti
         # df_filled = df_sampled.ffill()
 
         # Calculate mass flux
-        a = 1.4157 # Cross sectional area
+        a = 1.4137 # Cross sectional area
         total = (df_filled.sum(axis=1, min_count=1) / a).dropna()
         # print(total.reset_index().head(10))
 
@@ -293,7 +320,7 @@ def plot_mass_flux(df, time_range=None, highlight_range=None, events=None, x0_ti
         )
 
     ax.set_xlabel("Time since Ignition [s]")
-    ax.set_ylabel("Mass Flux [g/s * m^2]")
+    ax.set_ylabel("Mass Flux [kg/s * m^2]")
     ax.set_title("Mass Flux")
     ax.grid(True)
     ax.legend()
@@ -301,14 +328,86 @@ def plot_mass_flux(df, time_range=None, highlight_range=None, events=None, x0_ti
     plt.show()
 
 
+########### Function to plot normalized thurst ###########
+def plot_thrust(df, sequence_start, time_range=None, highlight_range=None, x0_time=None):
+    
+    if df.empty:
+        return
+    
+    df = df.copy()
+    df["time"] = pd.to_datetime(df["time"], errors="coerce")
+    sequence_start = pd.to_datetime(sequence_start)
+
+    # Remove initial offset: subtract mean of the 2 seconds BEFORE sequence_start
+    window_start = sequence_start - pd.Timedelta(seconds=2)
+    baseline_window = df[(df["time"] >= window_start) & (df["time"] < sequence_start)]
+
+    if not baseline_window.empty:
+        offset = baseline_window["value"].mean()
+        df["value"] = df["value"] - offset
+
+    # Filter according to time_range
+    if time_range is not None:
+        start, end = time_range
+        df = df[(df["time"] >= start) & (df["time"] <= end)]
+        if df.empty:
+            return
+
+    if x0_time is None:
+        x0_time = sequence_start
+    x0_time = pd.to_datetime(x0_time)
+
+    df["t_rel_s"] = (df["time"] - x0_time).dt.total_seconds()
+
+    # Plot normalized thrust
+    fig, ax = plt.subplots(figsize=(10, 4))
+    ax.plot(df["t_rel_s"], df["value"], label="Normalized Thrust")
+
+    # Sequence window (also relative)
+    if highlight_range is not None:
+        hs, he = highlight_range
+        hs_rel = (pd.to_datetime(hs) - x0_time).total_seconds()
+        he_rel = (pd.to_datetime(he) - x0_time).total_seconds()
+        ax.axvspan(hs_rel, he_rel, color="gray", alpha=0.3)
+
+    # Add vertical lines and sequnce text
+    if highlight_range is not None:
+        hs, he = highlight_range
+        hs_rel = (pd.to_datetime(hs) - x0_time).total_seconds()
+        he_rel = (pd.to_datetime(he) - x0_time).total_seconds()
+
+        ax.axvline(hs_rel, color="darkred", linestyle="--", linewidth=1.1, alpha=0.9)
+        ax.axvline(he_rel, color="navy", linestyle="--", linewidth=1.1, alpha=0.9)
+
+        ax.text(
+            hs_rel, 0.98, "Ignition",
+            transform=ax.get_xaxis_transform(),
+            rotation=90, va="top", ha="right", fontsize=8, color="darkred"
+        )
+        ax.text(
+            he_rel, 0.98, "Initiate Shutdown",
+            transform=ax.get_xaxis_transform(),
+            rotation=90, va="top", ha="right", fontsize=8, color="navy"
+        )
+
+    ax.set_xlabel("Time Since Ignition [s]")
+    ax.set_ylabel("Normalized Thrust [N]")
+    ax.set_title("Normalized Thrust\n(Offset correct with mean over 2 seconds before firing sequence initiation)", fontsize=10)
+    ax.grid(True)
+    ax.legend()
+    fig.tight_layout()
+    plt.show()
+
+
 ########### Function to plot equivalence ratio of system ###########
-def plot_isp(df, df_thr, time_range=None, highlight_range=None, x0_time=None):
+def plot_isp(df, df_thr, sequence_start, time_range=None, highlight_range=None, x0_time=None):
     if df.empty:
         return
 
     # Set zero to be begning of sequence    
     df = df.copy()
     df["time"] = pd.to_datetime(df["time"], errors="coerce")
+    sequence_start = pd.to_datetime(sequence_start)
 
     if time_range is not None:
         start, end = time_range
@@ -354,14 +453,13 @@ def plot_isp(df, df_thr, time_range=None, highlight_range=None, x0_time=None):
         df_thr = df_thr.rename(columns={"value": "thrust"})
         df_thr = df_thr.sort_values("time")[["time", "thrust"]]
 
-        # Remove initial offset: subtract mean of first 2 seconds
-        time_start = df_thr["time"].min()
-        time_2sec = time_start + pd.Timedelta(seconds=2)
-        first_2sec = df_thr[df_thr["time"] <= time_2sec]
+        # Remove initial offset: subtract mean of the 2 seconds before sequence_start
+        window_start = sequence_start - pd.Timedelta(seconds=2)
+        baseline_window = df_thr[(df_thr["time"] >= window_start) & (df_thr["time"] < sequence_start)]
 
-        if not first_2sec.empty:
-            offset = first_2sec["thrust"].mean()
-            df_thr["thrust"] = df_thr["thrust"] - offset            
+        if not baseline_window.empty:
+            offset = baseline_window["thrust"].mean()
+            df_thr["thrust"] = df_thr["thrust"] - offset          
 
         # Nearest join
         aligned = pd.merge_asof(
@@ -375,7 +473,7 @@ def plot_isp(df, df_thr, time_range=None, highlight_range=None, x0_time=None):
         # Clean up
         aligned = aligned.dropna(subset=["mdot", "thrust"])          
 
-        # Plot equivalence ratio
+        # Calculate and plot isp
         if not aligned.empty:
             g0 = 9.80665
             mdot_kg_s = aligned["mdot"] / 1000.0
@@ -383,7 +481,7 @@ def plot_isp(df, df_thr, time_range=None, highlight_range=None, x0_time=None):
             aligned["t_rel_s"] = (aligned["time"] - x0_time).dt.total_seconds()
 
             fig, ax = plt.subplots(figsize=(10, 4))
-            ax.plot(aligned["t_rel_s"], aligned["isp"], color="darkblue", label="Specific Impulse", linewidth=1.5)
+            ax.plot(aligned["t_rel_s"], aligned["isp"], label="Specific Impulse")
 
     # Sequence window (also relative)
     if highlight_range is not None:
